f9165e2db7368a2fd8488ba5669bf6cfdd7d8369
Zane Allen <zallen02@uafs.edu>
2026-08-24 09:08:30 -0500

    updated pretest

--- pretest/prob1/Prob1.java
+++ pretest/prob1/Prob1.java
@@ -1,10 +1,27 @@
 package prob1;
 
 public class Prob1 {
+	static void findMaxDifference() {
+		int[] x = {5,1,7,8,3};
+		for (int i=0; i < x.length; i++) {
+			int difference = x[i]-x[i+1];
+			if(difference<0) {
+				difference = (difference*difference)/(-difference);
+				//this is a very dumb way to go about getting the absolute value i know lol
+				
+			}
+			System.out.println(difference);
+
+			}
+		}
+		
 
 	public static void main(String[] args) {
-		
+		findMaxDifference();
+
+			
+		}
 
 	}
 
-}
+
--- pretest/prob2/Prob2.java
+++ pretest/prob2/Prob2.java
@@ -1,10 +1,27 @@
 package prob2;
 
 public class Prob2 {
+	static void countVowelSequences() {
+		String str = "Hello World";
+		int counter = 0;
+		for(int i = 0; i < str.length(); i++) {
+			str = str.toUpperCase();
+			if( str.charAt(i)="a" ) {
+				counter++;
+			}else if( str.charAt(i)="e" ) {
+				counter++;
+			}else if( str.charAt(i) ="i" ) {
+				counter++;
+			}else if( str.charAt(i) ="o" ) {
+				counter++;
+			}else if( str.charAt(i) ="u" ) {
+				counter++;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		
-
+		countVowelSequences();
 	}
 
 }
